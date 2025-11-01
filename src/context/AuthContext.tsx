7aa45b0a382aad0722f81fd2fbed3dba267c5b92
jaynip-cloud <jaynip@whitelabeliq.com>
2025-11-01 21:39:11 +0530

    Added AuthContext.tsx

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,6 @@
+
+      console.log('AuthContext: Loading user data for:', currentUser.email);
+      console.log('AuthContext: Organizations loaded:', orgs);
+        console.log('AuthContext: Setting organization:', orgs[0]);
+      } else {
+        console.log('AuthContext: No organizations found');
